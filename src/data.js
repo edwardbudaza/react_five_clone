38ec235c28137248e9753a3f0447a20a5e8c4778
edwardbudaza <edward.budaza@yahoo.co.za>
2023-05-16 07:42:11 +0200

    installed infinite-react-carousel for slider and created components for category card and slide component

---
+++ src/data.js
@@ -0,0 +1,64 @@
+export const cards = [
+    {
+      id: 1,
+      title: "AI Artists",
+      desc: "Add talent to AI",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209532/popular/ai-artists_wmfgna.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 2,
+      title: "Logo Design",
+      desc: "Build yor brand",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/logo-design_ep4uls.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 3,
+      title: "WordPress",
+      desc: "Customize your site",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209532/popular/wordpress_jennga.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 4,
+      title: "Voice Over",
+      desc: "Share your message",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209532/popular/voiceover_eavdch.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 5,
+      title: "Video Explainer",
+      desc: "Engage your audience",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/animated-explainer_nmipli.jpg?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 6,
+      title: "Social Media",
+      desc: "Reach more customers",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/social_xkvxi7.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 7,
+      title: "SEO",
+      desc: "Unlock growth online",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/seo_df3ibg.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+      id: 8,
+      title: "Illustration",
+      desc: "Color you dreams",
+      img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/illustration_kxoywi.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+    },
+    {
+        id: 9,
+        title: "Data Entry",
+        desc: "Learn your business",
+        img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209531/popular/data-entry_xnmfsz.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      },
+      {
+        id: 10,
+        title: "Translation",
+        desc: "Go Global",
+        img: "https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684209532/popular/translation_rxohg2.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load",
+      },
+      
+  ];
+  
