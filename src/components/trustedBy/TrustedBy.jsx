fa6955bc80027fbd52f8a9a4c5161b474f895724
edwardbudaza <edward.budaza@yahoo.co.za>
2023-05-16 05:18:26 +0200

    added trusted by component, which includes images that are hosted on cloudinary

---
+++ src/components/trustedBy/TrustedBy.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './TrustedBy.scss'
+
+const TrustedBy = () => {
+  return (
+    <div className='trustedBy'>
+      <div className="container">
+        <span>Trusted by:</span>
+        <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684206073/trustedby/pandg_m4fl0x.png" alt="" />
+        <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684206073/trustedby/paypal_yllwgw.png" alt="" />
+        <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684206073/trustedby/meta_j7rgbt.png" alt="" />
+        <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684206073/trustedby/netflix_gkptxj.png" alt="" />
+        <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684206073/trustedby/google_mlrmdg.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default TrustedBy
