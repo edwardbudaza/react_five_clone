e99b11854b65cdf76131097c828698152af4503b
edwardbudaza <edward.budaza@yahoo.co.za>
2023-05-20 03:23:00 +0200

    added project card component and styling - and included it on home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import "./Home.scss";
+
 import Featured from '../../components/featured/Featured';
 import TrustedBy from '../../components/trustedBy/TrustedBy';
 import Slide from '../../components/slide/Slide';
-import { cards } from '../../data'
 import CategoryCard from '../../components/categoryCard/CategoryCard';
+import ProjectCard from '../../components/projectCard/ProjectCard';
+
+import { cards, projects } from '../../data'
 
 const Home = () => {
   return (
@@ -43,10 +46,49 @@ const Home = () => {
             <p>Our round-the-clock support team is available to help anytime, anywhere.</p>
           </div>
           <div className="item">
-            <video src="https://res.cloudinary.com/dpmd0fdsa/video/upload/v1684215214/video/video_qhaddo.mp4" controls></video>
+            <video src="https://res.cloudinary.com/dpmd0fdsa/video/upload/v1684215214/video/video_qhaddo.mp4?auto=compress&cs=tinysrgb&w=1600&lazy=load" controls></video>
           </div>
         </div>
       </div>
+
+
+      <div className='features dark'>
+        <div className="container">
+          <div className="item">
+            <h1>fiverr business</h1>
+            <h1>A business solution designed for teams</h1>
+            <p>Upgrade to a curated experience to access vetted talent and exclusive tools</p>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Talent matching
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Dedicated account management
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Team collaboration tools
+            </div>
+            <div className="title">
+              <img src="./img/check.png" alt="" />
+              Business payment solutions
+            </div>
+            <button>Explore Fiverr Business</button>
+          </div>
+          <div className="item">
+            <img src="https://res.cloudinary.com/dpmd0fdsa/image/upload/v1684218040/business/business-desktop_nxi5ho.webp?auto=compress&cs=tinysrgb&w=1600&lazy=load" />
+          </div>
+        </div>
+      </div>
+
+      <Slide slidesToShow={4} arrowsScroll={4}>
+        {projects.map(project => (
+          <ProjectCard key = {project.id} item={project}/>
+        ))}
+      </Slide>
+
+
     </div>
   )
 }
